8c4dd44df745d5bcd2c54c7d9085144741d99985
lwhitelock <79275328+lwhitelock@users.noreply.github.com>
2022-05-17 23:28:40 +0100

    Fixed milliseconds

--- Modules/DattoRMMGenerator.psm1
+++ Modules/DattoRMMGenerator.psm1
@@ -328,7 +328,7 @@ function Get-DRMMAlertHistorySection {
 
     $ParsedOpenAlerts = $DeviceOpenAlerts | ForEach-Object {
         $Timestamp = $_.timestamp
-        $Time = $((Get-Date 01.01.1970).AddSeconds($Timestamp))
+        $Time = $((Get-Date 01.01.1970).AddMilliSeconds($Timestamp))
         [PSCustomObject]@{
             View        = "<a class=`"button-a button-a-primary`" target=`"_blank`" href=`"https://$($DattoPlatform)rmm.centrastage.net/alert/$($_.alertUid)`" style=`"background: #333333; border: 1px solid #000000; font-family: sans-serif; font-size: 15px; line-height: 15px; text-decoration: none; padding: 13px 17px; color: #ffffff; display: block; border-radius: 4px;`">View</a>"
             Priority    = $_.priority
@@ -343,7 +343,7 @@ function Get-DRMMAlertHistorySection {
 
     $ParsedResolvedAlerts = $DeviceResolvedAlerts | ForEach-Object {
         $Timestamp = $_.timestamp
-        $Time = $((Get-Date 01.01.1970).AddSeconds($Timestamp))
+        $Time = $((Get-Date 01.01.1970).AddMilliSeconds($Timestamp))
         [PSCustomObject]@{
             View        = "<a class=`"button-a button-a-primary`" target=`"_blank`" href=`"https://$($DattoPlatform)rmm.centrastage.net/alert/$($_.alertUid)`" style=`"background: #333333; border: 1px solid #000000; font-family: sans-serif; font-size: 15px; line-height: 15px; text-decoration: none; padding: 13px 17px; color: #ffffff; display: block; border-radius: 4px;`">View</a>"
             Priority    = $_.priority
